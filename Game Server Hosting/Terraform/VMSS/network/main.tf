b69f7612698fa92a42563a49a94b2e8b32ed8703
Charles Shea <chashea@microsoft.com>
2022-11-30 14:59:01 -0500

    update files

---
+++ Game Server Hosting/Terraform/VMSS/network/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  # In modules we should only specify the min version
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  alias           = "hub"
+  subscription_id = var.hub_subscription_id
+}
+
+provider "azurerm" {
+  features {}
+  alias           = "spoke"
+  subscription_id = var.spoke_subscription_id
+}
