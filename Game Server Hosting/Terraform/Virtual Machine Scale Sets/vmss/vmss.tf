0e25c281534f92d9454ad00c2c8011fed642a9be
Charles Shea <chashea@microsoft.com>
2022-12-16 15:48:08 -0500

    update agw module

--- Game Server Hosting/Terraform/Virtual Machine Scale Sets/vmss/vmss.tf
+++ Game Server Hosting/Terraform/Virtual Machine Scale Sets/vmss/vmss.tf
@@ -1,18 +1,18 @@
 resource "azurerm_windows_virtual_machine_scale_set" "vmss" {
-  count               = var.vmss_count
-  name                = var.vmss_name
-  resource_group_name = var.rg_vmss
-  location            = var.resource_location
-  sku                 = var.vmss_sku
-  instances           = 2
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
-  zone_balance        = true
-  zones               = ["1", "2", "3"]
-  enable_automatic_updates = true
-  upgrade_mode        = "Automatic"
+  count                      = var.vmss_count
+  name                       = var.vmss_name
+  resource_group_name        = var.rg_vmss
+  location                   = var.resource_location
+  sku                        = var.vmss_sku
+  instances                  = 2
+  admin_username             = var.admin_username
+  admin_password             = var.admin_password
+  zone_balance               = true
+  zones                      = ["1", "2", "3"]
+  enable_automatic_updates   = true
+  upgrade_mode               = "Automatic"
   encryption_at_host_enabled = true
-  tags                = var.resource_tags
+  tags                       = var.resource_tags
 
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
