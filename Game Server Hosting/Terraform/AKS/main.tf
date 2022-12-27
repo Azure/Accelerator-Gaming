5be54bab5c7489dfcb60bce09770d2283e156fc3
Charles Shea <chashea@microsoft.com>
2022-12-27 13:49:59 -0500

    update main and variables files

---
+++ Game Server Hosting/Terraform/AKS/main.tf
@@ -0,0 +1,22 @@
+module "network" {
+  source = "./network"
+  # Resource Group
+  rg_spoke          = var.rg_spoke
+  resource_location = var.resource_location
+  resource_tags     = var.resource_tags
+  rg_hub            = var.rg_hub
+  #Subscriptions
+  hub_subscription_id   = var.hub_subscription_id
+  spoke_subscription_id = var.spoke_subscription_id
+  # Virtual Network
+  hub_vnet_name            = var.hub_vnet_name
+  spoke_vnet_name          = var.spoke_vnet_name
+  spoke_vnet_address_space = var.spoke_vnet_address_space
+  spoke_subnet_name        = var.spoke_subnet_name
+  subnet_address_prefix    = var.subnet_address_prefix
+  nsg_name                 = var.nsg_name
+  peer1_name               = var.peer1_name
+  peer2_name               = var.peer2_name
+
+
+}
