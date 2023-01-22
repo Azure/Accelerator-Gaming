5cc90021f8b3be953a5d43cb09744f30751f8836
cshea15 <chashea@micrsoft.com>
2023-01-22 16:30:35 -0500

    update vmss file

--- Game Server Hosting/Terraform/Virtual Machine Scale Sets/vmss/vmss.tf
+++ Game Server Hosting/Terraform/Virtual Machine Scale Sets/vmss/vmss.tf
@@ -15,27 +15,20 @@ resource "azurerm_windows_virtual_machine_scale_set" "vmss" {
   admin_password           = var.admin_password
   zone_balance             = true
   zones                    = ["1", "2", "3"]
-  enable_automatic_updates = false /*Leave false with automatic os update policy set to true*/
-  upgrade_mode             = "Automatic"
-  automatic_os_upgrade_policy {
-    disable_automatic_rollback  = false
-    enable_automatic_os_upgrade = true
-  }
+  enable_automatic_updates = true /*Leave false with enable automatic os update policy set to true*/
+  upgrade_mode             = "Automatic" /*Other choices: Manual or Rolling*/
   encryption_at_host_enabled = true
   tags                       = var.resource_tags
-
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
     sku       = "2019-Datacenter"
     version   = "latest"
   }
-
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-
   network_interface {
     name                          = "nic-vmss-${var.prefix}-${count.index + 1}"
     primary                       = true
@@ -49,4 +42,3 @@ resource "azurerm_windows_virtual_machine_scale_set" "vmss" {
   }
   license_type = "Windows_Server"
 }
-
